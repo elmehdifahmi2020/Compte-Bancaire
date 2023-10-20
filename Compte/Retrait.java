880eec6c00b182aba85ef13fc5bb011996b3c2a4
ElMehdi Fahmi <113394694+elmehdifahmi2020@users.noreply.github.com>
2023-10-20 16:52:42 +0100

    Delete Compte/Retrait.java

--- Compte/Retrait.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-public class Retrait extends Operation{
-    public Retrait(Integer numero, Date dateOp, double montant){
-        super(numero,dateOp,montant);
-
-    }
-
-}
